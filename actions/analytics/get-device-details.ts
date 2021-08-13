b5f98cded2006ea8b8e25e65cdbe4cd10b468da7
theophilus ukuyoma <theo4biz2012@gmail.com>
2021-08-14 00:43:54 +0300

    Fix: orientation issues in get device type

--- actions/analytics/get-device-details.ts
+++ actions/analytics/get-device-details.ts
@@ -1,9 +1,9 @@
 const getDeviceType = () => {
-  const { width } = window.screen
+  const width = window?.screen?.width
   const normalOrientation = () => {
     if (
-      window.screen.orientation.type === 'portrait-primary' ||
-      window.screen.orientation.type === 'portrait-secondary'
+      window?.screen?.orientation?.type === 'portrait-primary' ||
+      window?.screen?.orientation?.type === 'portrait-secondary'
     ) {
       return true
     }
@@ -15,13 +15,13 @@ const getDeviceType = () => {
 }
 
 export default function getDeviceDetails() {
-  const { cookieEnabled, platform, language } = window.navigator
-  const { height, width, orientation } = window.screen
+  const { cookieEnabled, platform, language } = window?.navigator
+  const { height, width, orientation } = window?.screen
   return {
     deviceType: getDeviceType(),
     language,
     platform,
-    orientation: orientation.type,
+    orientation: orientation?.type ?? 'landscape-primary',
     height,
     width,
     cookieEnabled,
